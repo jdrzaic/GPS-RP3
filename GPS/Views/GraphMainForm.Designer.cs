2e859d8efc4d4ff04805bce61da8f83cd9aa113e
Jelena Drzaic <jelena.drzaic1@gmail.com>
2017-02-17 11:51:27 -0800

    add dialogs

--- GPS/Views/GraphMainForm.Designer.cs
+++ GPS/Views/GraphMainForm.Designer.cs
@@ -28,11 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNodeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calculateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shortestPathToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shortestWithCriteriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userManualToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cleanGraphToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem,
+            this.calculateToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNodeToolStripMenuItem,
+            this.cleanGraphToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // addNodeToolStripMenuItem
+            // 
+            this.addNodeToolStripMenuItem.Name = "addNodeToolStripMenuItem";
+            this.addNodeToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.addNodeToolStripMenuItem.Text = "Add Node";
+            // 
+            // calculateToolStripMenuItem
+            // 
+            this.calculateToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.shortestPathToolStripMenuItem,
+            this.shortestWithCriteriaToolStripMenuItem});
+            this.calculateToolStripMenuItem.Name = "calculateToolStripMenuItem";
+            this.calculateToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.calculateToolStripMenuItem.Text = "Calculate";
+            // 
+            // shortestPathToolStripMenuItem
+            // 
+            this.shortestPathToolStripMenuItem.Name = "shortestPathToolStripMenuItem";
+            this.shortestPathToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.shortestPathToolStripMenuItem.Text = "Shortest Path";
+            // 
+            // shortestWithCriteriaToolStripMenuItem
+            // 
+            this.shortestWithCriteriaToolStripMenuItem.Name = "shortestWithCriteriaToolStripMenuItem";
+            this.shortestWithCriteriaToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.shortestWithCriteriaToolStripMenuItem.Text = "Shortest Path with Criteria";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.userManualToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // userManualToolStripMenuItem
+            // 
+            this.userManualToolStripMenuItem.Name = "userManualToolStripMenuItem";
+            this.userManualToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.userManualToolStripMenuItem.Text = "User manual";
+            // 
+            // cleanGraphToolStripMenuItem
+            // 
+            this.cleanGraphToolStripMenuItem.Name = "cleanGraphToolStripMenuItem";
+            this.cleanGraphToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.cleanGraphToolStripMenuItem.Text = "Clean Graph";
+            // 
+            // GraphMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "GraphMainForm";
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.menuStrip2);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Name = "GraphMainForm";
+            this.Text = "Edit Graph";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNodeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calculateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem shortestPathToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem shortestWithCriteriaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem userManualToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cleanGraphToolStripMenuItem;
     }
 }
---
+++ GPS/Views/LocationNodeButton.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPS.Views
+{
+    class LocationNodeButton : RadioButton
+    {
+        private GPSNode node;
+        public LocationNodeButton() : base()
+        {
+            this.Text = "";
+            OuterColor = Color.Red;
+            InnerColor = Color.Red;
+            this.AutoSize = false;
+            Font = new Font("Arial", 8.75F);
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            BackColor = Color.Transparent;
+        }
+        public Color InnerColor { get; set; }
+        public Color OuterColor { get; set; }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            Graphics g = pevent.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            PaintParentBackground(pevent);
+            int diameter = ClientRectangle.Height - 1;
+
+            RectangleF innerRect = new RectangleF(1F, 1F, diameter - 2, diameter - 2);
+            g.FillEllipse(new SolidBrush(Color.Red), innerRect);
+
+            Rectangle outerRect = new Rectangle(0, 0, diameter, diameter);
+            g.DrawEllipse(new Pen(OuterColor), outerRect);
+
+            if (Checked)
+            {
+                innerRect.Inflate(-3, -3);
+                g.FillEllipse(new SolidBrush(InnerColor), innerRect);
+            }
+
+            g.DrawString(Text, Font, new SolidBrush(ForeColor), diameter + 5, 1);
+        }
+
+        private void PaintParentBackground(PaintEventArgs e)
+        {
+            if (Parent == null)
+            {
+                e.Graphics.FillRectangle(SystemBrushes.Control, ClientRectangle);
+                return;
+            }
+
+
+            Rectangle rect = new Rectangle(Left, Top, Width, Height);
+            e.Graphics.TranslateTransform(-rect.X, -rect.Y);
+
+            try
+            {
+                using (PaintEventArgs pea = new PaintEventArgs(e.Graphics, rect))
+                {
+                    pea.Graphics.SetClip(rect);
+                    InvokePaintBackground(Parent, pea);
+                    InvokePaint(Parent, pea);
+                }
+            }
+            finally
+            {
+                e.Graphics.TranslateTransform(rect.X, rect.Y);
+            }
+        }
+    }
+}
